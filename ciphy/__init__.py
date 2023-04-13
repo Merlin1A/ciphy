7ba01b6b478e88aef49d8e1d6a4e855e04463030
Jesse Brookins <jessebrookins@protonmail.com>
2023-04-13 18:24:07 -0400

    Config fixes

--- ciphy/__init__.py
+++ ciphy/__init__.py
@@ -15,7 +15,6 @@ class PasswordManager:
    def __del__(self):
       del self.password_generator
       del self.AESCipher
-      del self.password
       gc.collect()
 
    def gen_pass(self, password_length=8, num_pseudo_words=1, num_passwords=1):
@@ -30,7 +29,6 @@ class PasswordManager:
       password = getpass.getpass("Enter your password: ")
       
       self.AESCipher.encrypt_file(in_filename, out_filename, override, password)
-    
 
    def decrypt(self, in_filename, out_filename=None):
       password = getpass.getpass("Enter your password: ")
@@ -40,4 +38,6 @@ class PasswordManager:
    
 def main():
     fire.Fire(PasswordManager)
+
+main()
 
